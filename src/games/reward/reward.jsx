4c5334d614fdaa6a0befded0eff8755e9822cfce
ZriouilAnas <120396174+ZriouilAnas@users.noreply.github.com>
2025-05-27 15:19:23 +0100

    Refactor

--- src/games/reward/reward.jsx
+++ src/games/reward/reward.jsx
@@ -53,33 +53,7 @@ function Reward() {
           padding: "10px",
         }}
         className="reward-container"
-      >
-        <h1>IP : 197.879.198.1 </h1>
-        <h1>Location : Atrantica </h1>
-        <h1>Zip : 42069 </h1>
-        <h1>N : 43.7262 </h1>
-        <h1>W : 12.4818</h1>
-        <h1>SS Number : 6079198516176524</h1>
-        <h1>ipv6 : 2001:db8:3333:4444:5555:6666:7777:8888</h1>
-        <h1>upnp : enabled </h1>
-        <h1>dmz : 10.11.12.45</h1>
-        <h1>mac : 5A:38:3e:b9:99</h1>
-        <h1>dns : 8.8.8.8</h1>
-        <h1>alt dns : 1.1.1.0</h1>
-        <h1>ISP : SuperServer</h1>
-        <h1>User Agent: Chrome/122.0.9.0</h1>
-        <h1>Device: Desktop</h1>
-        <h1>Architecture: 64 bits</h1>
-        <h1>Browser: Chrome Generic</h1>
-        <h1>Memory: 32684MB RAM</h1>
-        <h1> Windows Dir: C:\WINDOWS</h1>
-        <h1> DirectX Version: DirectX 12</h1>
-        <h1>Native Mode: 1920 x 1080(p)</h1>
-        <h1>
-          Processor: Inter i7 14800U with GTX 1850 Mobile (16 CPUs), ~8.9GHz
-          ~16gb vram
-        </h1>
-      </div>
+      ></div>
     </>
   );
 }
